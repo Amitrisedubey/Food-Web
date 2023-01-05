c42c06d8ea7757c6983ab7077df8bad5206ab9ba
Amitrisedubey <amitrisedubey@gmail.com>
2023-01-05 13:58:32 +0530

    assign

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return ` <div id="home">
+    <h1><a href="./food.html">Home</a></h1>
+    <div id="option">
+        <h3><a href="./search.html">Search</a></h3>
+        <h3><a href="./receipe_day.html">Recipe of the Day</a></h3>
+        <h3><a href="./latest.html">Latest Recipe</a></h3>
+    </div>
+</div>`
+}
+export default navbar
